8c29e5a9070ca15ebe35d51c36f49c75f5ec7865
Pavel Rybakin <pvrybakin@gmail.com>
2024-10-20 01:52:23 +0300

    feat(Infrastructure): added category profile

---
+++ Infrastructure/Mapping/CategoryProfile.cs
@@ -0,0 +1,16 @@
+﻿using Application.Dto;
+using AutoMapper;
+using Core.Entities;
+
+namespace Infrastructure.Mapping;
+
+public class CategoryProfile: Profile
+{
+	public CategoryProfile()
+	{
+		CreateMap<CreateCategoryDto, Category>();
+		CreateMap<UpdateCategoryDto, Category>()
+			.ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
+	}
+	
+}
