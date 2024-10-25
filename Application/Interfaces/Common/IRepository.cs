05d25c1eb972d0097b201ee998d4928101f0ae55
Pavel Rybakin <pvrybakin@gmail.com>
2024-10-25 11:05:43 +0300

    feat(Application): created repository interface

---
+++ Application/Interfaces/Common/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Interfaces.Common;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+	Task<List<TEntity>> FindAllAsync();
+	Task<List<TEntity>> FindByIdsAsync(IEnumerable<Guid> ids);
+	Task<TEntity?> FindBySlugAsync(string slug);
+	Task<TEntity> CreateAsync(TEntity entity);
+	Task<TEntity> UpdateAsync(TEntity entity);
+	Task<TEntity> DeleteAsync(TEntity entity);
+}
